d94cbd36688d6ba0f92e6c3c1d34088d2e5d0a93
Erkebulan <kalkadamov14@gmail.com>
2024-05-18 08:50:01 +0500

    update

--- Kinokz/app/src/main/java/com/example/kinokz/fragment/MovieListFragment.kt
+++ Kinokz/app/src/main/java/com/example/kinokz/fragment/MovieListFragment.kt
@@ -5,15 +5,12 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.kinokz.R
 import com.example.kinokz.adapter.MovieAdapter
 import com.example.kinokz.adapter.MovieListAdapter
 import com.example.kinokz.databinding.FragmentMovieListBinding
